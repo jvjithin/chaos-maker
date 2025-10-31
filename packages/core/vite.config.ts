4d208bcd70f63e032c2c83034030567dfd0251b6
Jithin Vishnu R <jvjithin2@gmail.com>
2025-10-31 14:28:22 +0530

    build fixes

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,30 +4,24 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      // We only define the entry and global name here
       entry: 'src/index.ts',
       name: 'ChaosMaker',
     },
     rollupOptions: {
-      // We explicitly define our two output formats as an array
       output: [
         {
-          // This is the ES Module build
           format: 'es',
           entryFileNames: 'chaos-maker.js',
           dir: './dist',
         },
         {
-          // This is the UMD build
           format: 'umd',
-          // UMD builds must have a 'name'
           name: 'ChaosMaker', 
           entryFileNames: 'chaos-maker.umd.js',
           dir: './dist',
         },
       ],
     },
-    // Build to core's own dist directory
     outDir: './dist',
     emptyOutDir: true, 
   },
